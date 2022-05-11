5b71578ea8108b0653d54b86a90dc5d5ab15a424
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2022-05-11 11:37:30 -0300

    create components

---
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -0,0 +1,26 @@
+import { feedbackTypes } from ".."
+
+interface FeedbackTypeStepProps {
+  onFeedbackTypeChanged: (type: FeedbackType) => void;
+
+export function FeedbackTypeStep(props: FeedbackTypeStepProps) {
+  return (
+       <div className="flex py-8 gap-2 w-full">
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-violet-500 focus:border-violet-500 focus:outline-none"
+                onClick={() => {
+                  props.onFeedbackTypeChanged(key as FeedbackType)
+                }}
+                type="button"
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            )
+          })}
+        </div>  
+  )
+}

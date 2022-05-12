0a4805c087a18d8e01d59a56ff0aa43ba03da5ad
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2022-05-12 14:38:01 -0300

    add component

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import ideaImageUrl from '../../assets/idea.svg'
 import thoughtImageUrl from '../../assets/thought.svg'
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
   BUG: {
@@ -37,15 +38,9 @@ export function WidgetForm() {
 
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto"> 
-    <header>
-      <span className="text-xl leading-6">Deixe seu Feedback</span>
-      <CloseButton/>
-    </header>
-
-    {!feedbackType ? (
-      <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
-      ): (
-        <p>Hello World</p>
+    
+    {!feedbackType ? (<FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>) : (
+        <FeedbackContentStep feedbackType={feedbackType}/>
       )}
 
       <footer className="text-xs text-neutral-400">
